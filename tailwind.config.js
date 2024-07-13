186e6a0a50904283ca68272a22a59e5859ff27db
rinklepoonia <rinklepoonia889@gmail.com>
2024-07-13 11:28:53 +0530

    deploying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*/.{html,js}", "./index.html"],
+  content: ["./*/.{html,js}", "./footer.html"],
   theme: {
     fontSize: {
       "sm": "16px",
