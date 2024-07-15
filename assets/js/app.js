79d8561c3dcd436d8db128c0a722ad843f39dbc8
Shiv-468 <shivmuwal.147@gmail.com>
2024-07-15 09:02:49 +0530

    responsive done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,27 @@
-$('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    variableWidth:true,
-    centerMode: true,
-    // centerPadding: '10px',
-    focusOnSelect: true
-});
+
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     fade: true,
-    asNavFor: '.slider-nav'
+    asNavFor: '.slider-nav',
+    prevArrow: $('.arrow-left'),
+    nextArrow: $('.arrow-right')
+    
 });
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    autoPlay:true,
+    speed:500,
+    autoplaySpeed:5000,
+    variableWidth:true,
+    centerMode: true,
+    centerPadding: '0',
+    focusOnSelect: true,
+    prevArrow: $('.arrow-left'),
+    nextArrow: $('.arrow-right')
+});
+
